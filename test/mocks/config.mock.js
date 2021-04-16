28344c89a7ae45074b55c44937156e501badf6aa
Shiranuit <shiranui007@gmail.com>
2021-04-16 16:51:07 +0200

    add test folder

---
+++ test/mocks/config.mock.js
@@ -0,0 +1,10 @@
+
+class ConfigurationMock {
+    constructor() {
+      this.syncInterval = 7500;
+      this.systemMetricsInterval = 5000;
+    }
+  }
+  
+  module.exports = ConfigurationMock;
+  
---
+++ test/mocks/request.mock.js
@@ -0,0 +1,16 @@
+class RequestMock {
+    constructor() {
+      this.context = {};
+      this.input = {};
+      this.result = {};
+      this.response = {};
+      this.timestamp = Date.now()
+    }
+  
+    init(args) {
+      Object.assign(this, args);
+    }
+  }
+  
+  module.exports = RequestMock;
+  
