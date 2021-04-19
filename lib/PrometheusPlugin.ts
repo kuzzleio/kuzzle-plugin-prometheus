9d27dd0d4ddeb1d2b69e75bd6baa5ec312880aa7
Shiranuit <shiranui007@gmail.com>
2021-04-19 13:04:54 +0200

    add copyright comment in plugin

--- lib/PrometheusPlugin.ts
+++ lib/PrometheusPlugin.ts
@@ -1,3 +1,25 @@
+/*
+ * Kuzzle, a backend software, self-hostable and ready to use
+ * to power modern apps
+ *
+ * Copyright 2015-2021 Kuzzle
+ * mailto: support AT kuzzle.io
+ * website: http://kuzzle.io
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import {
   Plugin,
   PluginContext,
@@ -6,10 +28,31 @@ import {
 
 import { PrometheusController } from './controllers';
 
+/**
+ * @class PrometheusPlugin
+ *
+ * @property {KuzzlePluginContext}     context        Kuzzle plugin context
+ * @property {Object.<string, string>} hooks          Kuzzle plugin hooks
+ * @property {Routes}                  routes         Kuzzle plugin routes
+ * @property {Controllers}             controllers    Kuzzle plugin controllers
+ * @property {Prometheus.Registry}     registry       Prometheus Registry
+ * @property {Object.<string, Object>} kuzzleMetrics  Prometheus metrics dedicated for Kuzzle
+ * @property {String}                  key            Key used when pushing Prometheus Registry to Redis
+ * @property {String}                  hostname       Hostname
+ * @property {String}                  MAC            Host MAC address
+ * @property {String}                  IP             Host IPv4 adress
+ *
+ * @externs
+ */
 export class PrometheusPlugin extends Plugin {
   private defaultConfig: JSONObject;
   private prometheusController : PrometheusController;
 
+   /**
+   * PrometheusPlugin constructor
+   *
+   * @returns {PrometheusPlugin}
+   */
   constructor () {
     super({
       kuzzleVersion: '>=2.11.0 <3'
