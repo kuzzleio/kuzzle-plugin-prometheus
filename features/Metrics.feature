b10497e5c066e6068850ca33721b45ec523870cd
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2022-01-19 02:26:57 +0100

    Add Functional tests

--- features/Metrics.feature
+++ features/Metrics.feature
@@ -1,5 +1,20 @@
 Feature: Prometheus metrics fetching
-  Scenario: Fetching Prometheus formatted metrics from server:metrics
-    When I successfully execute the action "server":"metrics" with args:
-      | format   | "prometheus" |
-   Then I debug "result"
+  Scenario: Fetching Prometheus formatted metrics from server:metrics with the format parameter set to "prometheus"
+    Given A running Kuzzle instance at "localhost:7512"
+    When I send a HTTP request to "/_metrics?format=prometheus"
+    Then The HTTP response should be a Prometheus formatted metrics
+
+  Scenario: Trying to fetch Prometheus formatted metrics from server:metrics without the format parameter
+    Given A running Kuzzle instance at "localhost:7512"
+    When I send a HTTP request to "/_metrics"
+    Then The HTTP response should be a JSON object
+
+  Scenario: Trying to fetch Prometheus formatted metrics from server:metrics with the format parameter set to "whatEver"
+    Given A running Kuzzle instance at "localhost:7512"
+    When I send a HTTP request to "/_metrics?format=whatEver"
+    Then The HTTP response should be a JSON object
+  
+  Scenario: Trying to fetch Prometheus formatted metrics from server:metrics with the format parameter set to "prometheus" through WebSocket
+    Given A running Kuzzle instance at "localhost:7512"
+    When I send a WebSocket request to "server":"metrics" with the format parameter set to "prometheus"
+    Then The WebSocket response should be a JSON object with "200" status code and a "result.api.concurrentRequests" property
---
+++ features/step_definitions/plugin.steps.ts
@@ -0,0 +1,73 @@
+import { binding, given, then, when} from 'cucumber-tsflow';
+import { assert } from 'chai';
+import fetch from 'node-fetch';
+import { WebSocket } from 'ws';
+import _ from 'lodash';
+
+@binding()
+export class PluginSteps {
+  private host: string;
+  private result: any;
+
+  @given(/A running Kuzzle instance at "([^"]*)"/)
+  public async givenRunningKuzzleInstance(host: string) {
+      const response = await fetch(`http://${host}`);
+      assert(response.status === 200);
+      this.host = host;
+  }
+
+  @when(/I send a HTTP request to "([^"]*)"/)
+  public async whenISendARequestTo(url: string) {
+    const response = await fetch(`http://${this.host}${url}`);
+    this.result = response;
+  }
+
+  @when(/I send a WebSocket request to "([^"]*)":"([^"]*)" with the format parameter set to "([^"]*)"/)
+  public async whenISendAWebSocketRequestToWithTheFormatParameterSetTo(controller: string, action: string, format: string) {
+    const connection = new WebSocket(`ws://${this.host}`);
+    this.result = undefined;
+
+    connection.onmessage = (message) => {
+      this.result = message.data;
+      connection.close();
+    };
+    
+    connection.onopen = () => {
+      connection.send(JSON.stringify({
+        controller,
+        action,
+        format
+      }));
+    };
+
+    while (connection.readyState !== WebSocket.CLOSED && !this.result) {
+      await new Promise(resolve => setTimeout(resolve, 100));
+    }
+  }
+
+  @then(/The WebSocket response should be a JSON object with "([^"]*)" status code and a "([^"]*)" property/)
+  public async thenTheResponseShouldBeAJSONObjectWithAProperty(status: string, property: string) {
+    const json = JSON.parse(this.result);
+    assert(json.status === parseInt(status));
+    assert(_.get(json, property) !== undefined);
+  }
+
+  @then(/The HTTP response should be a JSON object/)
+  public async thenTheResponseShouldBeAJSONObject() {
+    assert(this.result !== null);
+    this.result = await this.result.json();
+    assert(typeof this.result === 'object');
+  }
+
+  @then(/The HTTP response should be a Prometheus formatted metrics/)
+  public async thenTheResponseShouldContainA() {
+    assert(this.result !== null);
+    this.result = await this.result.text();
+    assert(typeof this.result === 'string');
+
+    assert(this.result.startsWith('# HELP'));
+    assert(this.result.includes('kuzzle_api_concurrent_requests 1'))
+    assert(this.result.includes('kuzzle_network_connections{protocol="http/1.1"} 1'))
+  }
+
+}
